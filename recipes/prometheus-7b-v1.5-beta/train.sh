3ce94f53b5997c788274d3c5b78201de2223dc8a
Scott Suk <scottsuk0306@gmail.com>
2024-02-03 06:48:57 +0900

    Update train script

--- recipes/prometheus-7b-v1.5-beta/train.sh
+++ recipes/prometheus-7b-v1.5-beta/train.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-ACCELERATE_LOG_LEVEL=info accelerate launch --config_file recipes/accelerate_configs/multi_gpu.yaml --num_processes=1 scripts/run_sft.py recipes/prometheus-7b-v1.5-beta/sft/config_qlora.yaml --load_in_4bit=true
+# ACCELERATE_LOG_LEVEL=info accelerate launch --config_file recipes/accelerate_configs/multi_gpu.yaml --num_processes=1 scripts/run_sft.py recipes/prometheus-7b-v1.5-beta/sft/config_qlora.yaml --load_in_4bit=true
+
+ACCELERATE_LOG_LEVEL=info accelerate launch --config_file recipes/accelerate_configs/deepspeed_zero3.yaml scripts/run_sft.py recipes/prometheus-7b-v1.5-beta/sft/config_full_1.yaml
