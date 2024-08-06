83fc87b773eef043bca847d6515f12244095e2ad
Chansung Park <deep.diver.csp@gmail.com>
2024-08-07 02:03:28 +0900

    Update run_sft.py

--- scripts/run_sft.py
+++ scripts/run_sft.py
@@ -117,7 +117,7 @@ def main():
         torch_dtype=torch_dtype,
         use_cache=False if training_args.gradient_checkpointing else True,
         device_map=get_kbit_device_map() if quantization_config is not None else None,
-        quantization_config=quantization_config,
+        quantization_config=quantization_config.to_dict(),
     )
 
     model = model_args.model_name_or_path
